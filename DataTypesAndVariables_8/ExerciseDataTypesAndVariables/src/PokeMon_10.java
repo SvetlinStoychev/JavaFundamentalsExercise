4cb21655e9cc3c82997bd4ece009a147a33b46e2
SvetlinStoychev <filipovvenci@yahoo.com>
2023-05-20 20:13:52 +0300

    Update PokeMon_10.java

--- DataTypesAndVariables_8/ExerciseDataTypesAndVariables/src/PokeMon_10.java
+++ DataTypesAndVariables_8/ExerciseDataTypesAndVariables/src/PokeMon_10.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class PokeMon_10 {
     public static void main(String[] args) {
-        
+        Scanner reader = new Scanner(System.in);
+
+        int nPower = Integer.parseInt(reader.nextLine());
+        int startPower = nPower;
+        int mDistance = Integer.parseInt(reader.nextLine());
+        int yExhaustionFactor = Integer.parseInt(reader.nextLine());
+
+        int countOfTargets = 0;
+
+        while (nPower >= mDistance) {
+            nPower -= mDistance;
+            countOfTargets++;
+
+            if (nPower == startPower / 2) {
+                if (yExhaustionFactor != 0) {
+                    nPower /= yExhaustionFactor;
+                }
+            }
+        }
+
+        System.out.println(nPower);
+        System.out.println(countOfTargets);
     }
 }
