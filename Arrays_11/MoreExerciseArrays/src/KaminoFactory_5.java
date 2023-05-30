3abdc01a18ebfd314c19cb73a8ad35284cb1e6f4
SvetlinStoychev <filipovvenci@yahoo.com>
2023-05-30 21:47:10 +0300

    task decision

--- Arrays_11/MoreExerciseArrays/src/KaminoFactory_5.java
+++ Arrays_11/MoreExerciseArrays/src/KaminoFactory_5.java
@@ -10,16 +10,25 @@ public class KaminoFactory_5 {
         int resultSequenceCnt = 0;
         int resultStartIndex = -1;
         int[] resultSequence = new int[lengthSequences];
+        int resultSum = 0;
+        int bestSample = 1;
+        int bestSampleCnt = 1;
+        int maxSum = 0;
+        int maxSumSemple = 0;
+        int[] maxSumSempleArr = new int[lengthSequences];
 
         while (!userInput.equals("Clone them!")) {
 
             int[] currentSequence = new int[lengthSequences];
             int indexCnt = 0;
+            int currentSum = 0;
+
 
             for (int i = 0; i < userInput.length(); i++) {
 
                 if (userInput.charAt(i) == '1') {
                     currentSequence[indexCnt] = 1;
+                    currentSum++;
                     indexCnt++;
                 }  else if (userInput.charAt(i) == '0') {
                     currentSequence[indexCnt] = 0;
@@ -27,15 +36,23 @@ public class KaminoFactory_5 {
                 }
             }
 
-            int bestCntSequence = 0;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                maxSumSemple = bestSampleCnt;
+                maxSumSempleArr = currentSequence;
+            }
+
+            int bestCntSequence = 1;
             int bestStartIndex = -1;
             for (int i = 0; i < currentSequence.length; i++) {
 
                 int sequenceCnt = 1;
                 int sequenceStartIndex = i;
                 for (int j = i + 1; j < currentSequence.length; j++) {
-                    if (currentSequence[i] == currentSequence[j]) {
+                    if (currentSequence[i] == 1 && currentSequence[j] == 1) {
                         sequenceCnt++;
+                    } else {
+                        break;
                     }
                 }
 
@@ -46,19 +63,38 @@ public class KaminoFactory_5 {
             }
 
             if (bestCntSequence > resultSequenceCnt) {
-                resultSequenceCnt = bestStartIndex;
+                resultSequenceCnt = bestCntSequence;
                 resultStartIndex = bestStartIndex;
                 resultSequence = currentSequence;
+                resultSum = bestCntSequence;
+                bestSample = bestSampleCnt;
             } else if (bestCntSequence == resultSequenceCnt) {
                 if (bestStartIndex < resultStartIndex) {
                     resultStartIndex = bestStartIndex;
                     resultSequence = currentSequence;
-                }else if () {
-                    
+                    resultSum = bestCntSequence;
+                    bestSample = bestSampleCnt;
+                } else if (bestStartIndex == resultStartIndex) {
+                    if(maxSum > currentSum) {
+                        resultSum = maxSum;
+                        resultSequence = maxSumSempleArr;
+                        bestSample = maxSumSemple;
+                    } else {
+                        resultSum = currentSum;
+                        resultSequence = currentSequence;
+                        bestSample = bestSampleCnt;
+                    }
                 }
             }
 
+            bestSampleCnt++;
             userInput = reader.nextLine();
+        }
+
+        System.out.printf("Best DNA sample %d with sum: %d.%n", bestSample, resultSum);
+
+        for (int i = 0; i < resultSequence.length; i++) {
+            System.out.printf("%d ", resultSequence[i]);
         }
     }
 }
