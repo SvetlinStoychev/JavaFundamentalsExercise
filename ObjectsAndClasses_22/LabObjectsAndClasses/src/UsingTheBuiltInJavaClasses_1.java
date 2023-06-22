7d1f055bb64fdc56dc2b0df0d749e45835320bce
SvetlinStoychev <filipovvenci@yahoo.com>
2023-06-22 13:41:09 +0300

    task decision

---
+++ ObjectsAndClasses_22/LabObjectsAndClasses/src/UsingTheBuiltInJavaClasses_1.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class UsingTheBuiltInJavaClasses_1 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        String[] words = reader.nextLine().split(" ");
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < words.length; i++) {
+            int newPoss = rnd.nextInt(words.length);
+
+            String temp = words[i];
+            words[i] = words[newPoss];
+            words[newPoss] = temp;
+        }
+
+        System.out.println(String.join(System.lineSeparator(), words));
+    }
+}
