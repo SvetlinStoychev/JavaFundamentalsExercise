61c353b06d71e0744d155f5fc9328c38c5e9f39c
SvetlinStoychev <filipovvenci@yahoo.com>
2023-07-01 19:58:27 +0300

    task decision

--- AssociativeArrays_25/ExerciseAssociativeArraysLambdaAndStreamAPI/src/ForceBook_9.java
+++ AssociativeArrays_25/ExerciseAssociativeArraysLambdaAndStreamAPI/src/ForceBook_9.java
@@ -13,23 +13,31 @@ public class ForceBook_9 {
 
         while (!command.equals("Lumpawaroo")) {
 
-            ArrayList<String> currentUserList = new ArrayList<>();
-
             if (command.contains("|")) {
 
                 String[] commandsArr = command.split(" \\| ");
                 String forceSide = commandsArr[0];
                 String forceUser = commandsArr[1];
 
-                if ((!checkIfUserExistInMap(forceBookMap, forceUser)) && (!forceBookMap.containsKey(forceSide))) {
+                if (!forceBookMap.containsKey(forceSide)) {
 
-                    currentUserList.add(forceUser);
-                    forceBookMap.put(forceSide, currentUserList);
-                } else if ((!checkIfUserExistInMap(forceBookMap, forceUser)) && forceBookMap.containsKey(forceSide)) {
+                    forceBookMap.put(forceSide, new ArrayList<>());
+                }
 
-                    currentUserList = forceBookMap.get(forceSide);
-                    currentUserList.add(forceUser);
-                    forceBookMap.put(forceSide, currentUserList);
+                boolean isExist = false;
+
+                for (ArrayList<String> user : forceBookMap.values()) {
+
+                    if (user.contains(forceUser)) {
+
+                        isExist = true;
+                        break;
+                    }
+                }
+
+                if (!isExist) {
+
+                    forceBookMap.get(forceSide).add(forceUser);
                 }
 
             } else if (command.contains("->")) {
@@ -38,35 +46,17 @@ public class ForceBook_9 {
                 String forceUser = commandsArr[0];
                 String forceSide = commandsArr[1];
 
-                if ((!checkIfUserExistInMap(forceBookMap, forceUser)) && (!forceBookMap.containsKey(forceSide))) {
+                forceBookMap.entrySet().forEach(entryForceSide -> entryForceSide.getValue().remove(forceUser));
 
-                    currentUserList.add(forceUser);
-                    forceBookMap.put(forceSide, currentUserList);
+                if (!forceBookMap.containsKey(forceSide)) {
 
-                    System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
-
-                } else if ((!checkIfUserExistInMap(forceBookMap, forceUser)) && forceBookMap.containsKey(forceSide)) {
-
-                    currentUserList = forceBookMap.get(forceSide);
-                    currentUserList.add(forceUser);
-                    forceBookMap.put(forceSide, currentUserList);
-
-                    System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
-
-                } else if (checkIfUserExistInMap(forceBookMap, forceUser) && forceBookMap.containsKey(forceSide)) {
-
-                    if (!checkIfUserExistInList(forceBookMap.get(forceSide), forceUser)) {
-
-                        removeUserFromMap(forceBookMap, forceUser);
-                        currentUserList = forceBookMap.get(forceSide);
-                        currentUserList.add(forceUser);
-                        forceBookMap.put(forceSide, currentUserList);
-
-                        System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
-                    }
+                    forceBookMap.put(forceSide, new ArrayList<>());
                 }
-            }
 
+                forceBookMap.get(forceSide).add(forceUser);
+
+                System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
+            }
          command = reader.nextLine();
         }
 
@@ -82,39 +72,5 @@ public class ForceBook_9 {
                 }
             }
         }
-
-    }
-
-    private static void removeUserFromMap(LinkedHashMap<String, ArrayList<String>> map, String user) {
-
-        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
-
-            ArrayList<String> currentUsers = entry.getValue();
-
-            currentUsers.remove(user);
-        }
-    }
-    private static boolean checkIfUserExistInMap(LinkedHashMap<String, ArrayList<String>> map, String user) {
-
-        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
-
-            ArrayList<String> currentUsers = entry.getValue();
-
-            if (checkIfUserExistInList(currentUsers, user)) {
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean checkIfUserExistInList(ArrayList<String> usersList, String user) {
-
-        if (usersList.contains(user)) {
-            return true;
-        }
-
-        return false;
     }
 }
