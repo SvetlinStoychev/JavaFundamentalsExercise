125a9aca5f2c3c47eaf8947a9cd131d46b31b08c
SvetlinStoychev <filipovvenci@yahoo.com>
2023-07-03 20:01:14 +0300

    task decision

--- AssociativeArrays_25/More ExercisesAssociativeArraysLambdaAndStreamAPI/src/Judge_2.java
+++ AssociativeArrays_25/More ExercisesAssociativeArraysLambdaAndStreamAPI/src/Judge_2.java
@@ -8,12 +8,14 @@ public class Judge_2 {
 
         LinkedHashMap<String, Integer> contestParticipantsMap = new LinkedHashMap<>();
         LinkedHashMap<String, TreeMap<String, Integer>> contestUserPointsMap = new LinkedHashMap<>();
-        LinkedHashMap<String, Integer> userTotalPointsMap = new LinkedHashMap<>();
+        TreeMap<String, Integer> userTotalPointsMap = new TreeMap<>();
 
         while (!command.equals("no more time")) {
             String inputUserName = command.split(" -> ")[0];
             String inputContest = command.split(" -> ")[1];
             int inputPoints = Integer.parseInt(command.split(" -> ")[2]);
+            boolean isMorePointsThanBefore = false;
+            int pointsDifference = 0;
 
             if (!contestUserPointsMap.containsKey(inputContest)) {
                 TreeMap<String, Integer> currentUserPointsMap = new TreeMap<>();
@@ -30,6 +32,8 @@ public class Judge_2 {
                     if (inputPoints > currentPoints) {
                         currentUserPointsMap.put(inputUserName, inputPoints);
                         contestUserPointsMap.put(inputContest, currentUserPointsMap);
+                        isMorePointsThanBefore = true;
+                        pointsDifference = inputPoints - currentPoints;
                     }
                 } else {
                     currentUserPointsMap.put(inputUserName, inputPoints);
@@ -38,8 +42,11 @@ public class Judge_2 {
                     contestParticipantsMap.put(inputContest, contestParticipantsMap.get(inputContest) + 1);
                 }
             }
-
-            if (userTotalPointsMap.containsKey(inputUserName)) {
+            if (isMorePointsThanBefore) {
+                int currentPoints = userTotalPointsMap.get(inputUserName);
+                currentPoints += pointsDifference;
+                userTotalPointsMap.put(inputUserName, currentPoints);
+            } else if (userTotalPointsMap.containsKey(inputUserName)) {
                 int currentPoints = userTotalPointsMap.get(inputUserName);
                 currentPoints += inputPoints;
                 userTotalPointsMap.put(inputUserName, currentPoints);
@@ -53,6 +60,7 @@ public class Judge_2 {
         for (Map.Entry<String, TreeMap<String, Integer>> entry : contestUserPointsMap.entrySet()) {
             System.out.printf("%s: %d participants%n", entry.getKey(), contestParticipantsMap.get(entry.getKey()));
             ArrayList<Integer> points = new ArrayList<>();
+            //entry.getValue().descendingKeySet();
 
             for (Map.Entry<String, Integer> integerEntry : entry.getValue().entrySet()) {
                 points.add(integerEntry.getValue());
@@ -62,20 +70,50 @@ public class Judge_2 {
             Collections.reverse(points);
 
             int cnt = 1;
-            String lastPrinted = "";
+            List<String> printedList = new ArrayList<>();
 
             for (Integer point : points) {
 
                 for (Map.Entry<String, Integer> integerEntry : entry.getValue().entrySet()) {
 
-                    if (integerEntry.getValue() == point && (!integerEntry.getKey().equals(lastPrinted))) {
+                    if (integerEntry.getValue().equals(point) && (!printedList.contains(integerEntry.getKey()))) {
 
                         System.out.printf("%d. %s <::> %d%n", cnt, integerEntry.getKey(), integerEntry.getValue());
                         cnt++;
-                        lastPrinted = integerEntry.getKey();
+                        printedList.add(integerEntry.getKey());
+                        break;
                     }
+                }
+            }
+        }
+
+        System.out.println("Individual standings: ");
+
+        ArrayList<Integer> userPointsList = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : userTotalPointsMap.entrySet()) {
+            userPointsList.add(entry.getValue());
+        }
+
+        Collections.sort(userPointsList);
+        Collections.reverse(userPointsList);
+
+        int cnt = 1;
+        List<String> printedList = new ArrayList<>();
+
+        for (Integer userPoint : userPointsList) {
+
+            for (Map.Entry<String, Integer> entry : userTotalPointsMap.entrySet()) {
+
+                if (entry.getValue().equals(userPoint) && (!printedList.contains(entry.getKey()))) {
+
+                    System.out.printf("%d. %s -> %d%n", cnt, entry.getKey(), entry.getValue());
+                    cnt++;
+                    printedList.add(entry.getKey());
+                    break;
                 }
             }
         }
     }
 }
+
