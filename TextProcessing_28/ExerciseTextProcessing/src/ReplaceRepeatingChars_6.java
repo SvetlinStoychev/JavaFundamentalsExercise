08103faf4e478dd5cb950cbac07615ffdd91ac05
SvetlinStoychev <filipovvenci@yahoo.com>
2023-07-07 20:55:52 +0300

    task decision

--- TextProcessing_28/ExerciseTextProcessing/src/ReplaceRepeatingChars_6.java
+++ TextProcessing_28/ExerciseTextProcessing/src/ReplaceRepeatingChars_6.java
@@ -4,6 +4,22 @@ public class ReplaceRepeatingChars_6 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        
+        String inputText = reader.nextLine();
+        char[] inputArr = inputText.toCharArray();
+
+        StringBuilder resultText = new StringBuilder();
+
+        for (int i = 0; i < inputArr.length; i++) {
+
+            if (resultText.length() == 0) {
+                resultText.append(inputArr[i]);
+            }
+
+            if (resultText.charAt(resultText.length() - 1) != inputArr[i]) {
+                resultText.append(inputArr[i]);
+            }
+        }
+
+        System.out.println(resultText);
     }
 }
