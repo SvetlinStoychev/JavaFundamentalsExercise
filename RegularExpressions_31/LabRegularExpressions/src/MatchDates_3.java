2d07ddee9cf228bda57a7fccc899a2ab1f8a8ee1
SvetlinStoychev <filipovvenci@yahoo.com>
2023-07-11 15:39:43 +0300

    task decision

--- RegularExpressions_31/LabRegularExpressions/src/MatchDates_3.java
+++ RegularExpressions_31/LabRegularExpressions/src/MatchDates_3.java
@@ -1,24 +1,23 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchPhoneNumber_2 {
+public class MatchDates_3 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
         String input = reader.nextLine();
 
-        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+        String regex = "\\b(?<day>\\d{2})([.\\-\\/])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
-        ArrayList<String> result = new ArrayList<>();
-
         while (matcher.find()) {
-            result.add(matcher.group());
-        }
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
 
-        System.out.println(String.join(", ", result));
+            System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+        }
     }
 }
