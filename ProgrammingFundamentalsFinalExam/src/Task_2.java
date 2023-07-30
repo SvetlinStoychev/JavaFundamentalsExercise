a276673bd239c6c95a15609a1a28821f0e3d10ef
SvetlinStoychev <filipovvenci@yahoo.com>
2023-07-30 11:10:53 +0300

    task decision

--- ProgrammingFundamentalsFinalExam/src/Task_2.java
+++ ProgrammingFundamentalsFinalExam/src/Task_2.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task_2 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        String text = reader.nextLine();
+        String regex = "([@#]+)(?<color>[a-z]{3,})([@#]+)(\\W*)([\\/]+)(?<amount>[\\d]+)([\\/]+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
 
+        while (matcher.find()) {
+            String amount = matcher.group("amount");
+            String color = matcher.group("color");
+
+            System.out.printf("You found %s %s eggs!%n", amount, color);
+        }
     }
 }
